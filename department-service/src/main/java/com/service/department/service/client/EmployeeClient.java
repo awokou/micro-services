047eea0da7ab0e22ddbdc525bf7ada633ccde781
awokou <kawokou122@gmail.com>
2025-12-22 21:51:03 +0100

    Initial projet

--- department-service/src/main/java/com/service/department/service/client/EmployeeClient.java
+++ department-service/src/main/java/com/service/department/service/client/EmployeeClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "employee-service", url = "${employee.service.url}")
+@FeignClient(name = "employee-service", url = "${employees.service.url}")
 public interface EmployeeClient {
 
     @GetMapping
