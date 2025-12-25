f609e7a91fec62972c25b4edbe00680999996ee5
awokou <kawokou122@gmail.com>
2025-12-25 18:37:00 +0100

    Initial projet

--- product-service/src/main/java/com/server/product/service/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.server.product.service.repository;
-
-import com.server.product.service.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
